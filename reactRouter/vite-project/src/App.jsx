f0ebb47c036d82e857bae7fff0ed9e6da0f7880d
Jaydeep0342 <jaydeep0342@gmail.com>
2026-02-25 12:55:11 +0530

    React-router complete

--- reactRouter/vite-project/src/App.jsx
+++ reactRouter/vite-project/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="text-white p-4">
-    <h1 className=" bg-blue-500 text-2xl font-bold">Hello World</h1>
+    <div>
+      
     </div>
   )
 }
--- reactRouter/vite-project/src/main.jsx
+++ reactRouter/vite-project/src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './Home/Home.jsx'
+import About from './About/about.jsx'
+import Layout from './Layout.jsx'
+import Contact from './Contact/contact.jsx'
+import User from './User/User.jsx'
+import Github, { githubInfoLoader } from './Github/Github.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact/> },
+      { path: "/user/:userid", element: <User/> },
+      { path: "/github", element: <Github/>, loader: githubInfoLoader },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
