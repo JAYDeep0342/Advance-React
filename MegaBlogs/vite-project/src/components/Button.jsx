31d7572bde5a87ab463e801ea64067b4f92c8ac3
Jaydeep0342 <jaydeep0342@gmail.com>
2026-02-28 21:51:04 +0530

    MegaBlogs Funtionalities add

---
+++ MegaBlogs/vite-project/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Button = (
+    children,
+    type='button',
+    bgColor='blue-500',
+    textColor='white',
+    className='',
+    ...props
+) => {
+    return (
+        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+
+    )
+}
+
+export default Button;
---
+++ MegaBlogs/vite-project/src/components/container/container.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react';
+
+const container = () => {
+    return (
+        <div className='w-full mx-auto px-4'>
+            {children}
+        </div>
+
+        
+    )
+}
+
+export default container;
